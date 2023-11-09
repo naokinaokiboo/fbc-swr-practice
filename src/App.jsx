9403fa32a26266ee9d9f4ca4d293d7fab3b35c7f
naokinaokiboo <naokinaokiboo@gmail.com>
2023-11-09 10:51:24 +0900

    swrを使用してリクエスト状態、レスポンスによって表示内容を変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
-import { useState } from "react";
+import useSWR from "swr";
 import "./App.css";
 
 function App() {
   const url = "https://httpstat.us/200?sleep=2000";
   const headers = { Accept: "application/json" };
 
-  const [status, setStatus] = useState("");
+  const fetcher = async ([url, headers]) => {
+    const response = await fetch(url, { headers });
+    return response.json();
+  };
 
-  fetch(url, { headers })
-    .then((res) => res.json())
-    .then((json) => setStatus(json.description));
+  const { data, error, isLoading } = useSWR([url, headers], fetcher);
 
-  return <>{status && <p>Status : {status}</p>}</>;
+  if (error) return <p>Failed to load.</p>;
+  if (isLoading) return <p>Loading...</p>;
+
+  return <p>Status : {data.description}</p>;
 }
 
 export default App;
